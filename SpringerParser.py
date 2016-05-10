a7b2f6709ac7b1395efba5bf2d38593702e33c90
theisencr <theisen.cr@gmail.com>
2016-05-10 00:47:48 -0400

    committing the correct version

--- SpringerParser.py
+++ SpringerParser.py
@@ -3,29 +3,23 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 if len(sys.argv) != 2:
-  print('Usage: SpringerParser.py <output>')
+  print('Usage: SpringerParser.py <output>.tsv')
   exit()
 
 write_result = open(sys.argv[1], "w")
 driver = webdriver.Firefox()
 
+write_result.write("Title\tLink\n")
+
 for x in range(1, 6):
   driver.get("http://link.springer.com/search/page/" + str(x) + "?date-facet-mode=between&showAll=true&query=security+AND+%22entry+point%22+AND+%22attack+surface%22")
   assert "Springer" in driver.title
 
-  items = driver.find_elements_by_class_name('content-item-list')
+  items = driver.find_elements_by_class_name('title')
+  x = 0
   for item in items:
-    my_string = item.text
-    new_string = my_string.split("\n")
-    i = 0
-    for attr in new_string:
-      if (attr == "Chapter" or attr == "Article"):
-        write_result.write("")
-      elif (attr == "Look Inside Get Access"):
-        write_result.write("\n")
-      else:
-        write_result.write(attr + ",")
-      i += 1
+    print(item.get_attribute("href"))
+    write_result.write(item.text + "\t" + item.get_attribute("href") + "\n")
 
 driver.close()
 write_result.close()
