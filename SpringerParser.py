074c3b13a7d606ecdceae6e8281906d3c77d266c
Nuthan Munaiah <nuthan.munaiah@live.com>
2016-05-28 09:02:25 -0400

    Refactored the code to argumentize the command
    The script now takes the URL, indexing service, and other parameters as
    arguments to simplify extension to other indexing services. The Parser
    class is used to parse the actual results and the class is flexible
    enabling easy extension to other idexing services.

--- SpringerParser.py
+++ SpringerParser.py
@@ -1,25 +1,85 @@
+import argparse
+import csv
 import sys
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-if len(sys.argv) != 2:
-  print('Usage: SpringerParser.py <output>.tsv')
-  exit()
+from logger import *
+from parser import Parser
 
-write_result = open(sys.argv[1], "w")
-driver = webdriver.Firefox()
 
-write_result.write("Title\tLink\n")
+def parse(service, url, start, stop, browser):
+    results = list()
 
-for x in range(1, 6):
-  driver.get("http://link.springer.com/search/page/" + str(x) + "?date-facet-mode=between&showAll=true&query=security+AND+%22entry+point%22+AND+%22attack+surface%22")
-  assert "Springer" in driver.title
+    if '{}' not in url:
+        warning('URL does not have a placeholder for page number.')
 
-  items = driver.find_elements_by_class_name('title')
-  x = 0
-  for item in items:
-    print(item.get_attribute("href"))
-    write_result.write(item.text + "\t" + item.get_attribute("href") + "\n")
+    try:
+        parser = Parser(service, browser)
+        parser.setup()
 
-driver.close()
-write_result.close()
+        results.append(parser.get_header())
+        index = 1
+        for page in range(start, stop + 1):
+            results += parser.parse(url.format(page))
+            info('{} results after {} page(s)'.format(len(results) - 1, index))
+            index += 1
+    except KeyboardInterrupt:
+        sys.stdout.write('\r')
+        info('Exiting...')
+    finally:
+        parser.teardown()
+
+    return results
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+            description=(
+                'Script to scrape search results from indexing services.'
+            )
+        )
+    parser.add_argument(
+            '-b', dest='browser', default='firefox',
+            choices=['phantom', 'firefox'],
+            help='The browser to use when retrieving the search results.'
+        )
+    parser.add_argument(
+            '--start', dest='start', type=int, default=1,
+            help='Index of the page of results to start parsing from.'
+        )
+    parser.add_argument(
+            '--stop', dest='stop', type=int, default=1,
+            help='Index of the page of results to stop parsing to.'
+        )
+    parser.add_argument(
+            'service', choices=['ieee', 'acm', 'springer'],
+            help=(
+                'The indexing service from which the results are to be parsed.'
+            )
+        )
+    parser.add_argument(
+            'url',
+            help=(
+                'The URL of the search results. Use {} as the placeholder for '
+                'page number.'
+            )
+        )
+    parser.add_argument(
+            'output', help=(
+                'Path to the file to which the parse results should be '
+                'written.'
+            )
+        )
+    args = parser.parse_args()
+
+    info('Parsing {}'.format(args.url))
+    results = parse(
+            args.service, args.url, args.start, args.stop, args.browser
+        )
+    if results:
+        with open(args.output, 'w') as file_:
+            writer = csv.writer(file_)
+            writer.writerows(results)
+    info('Results written to {}'.format(args.output))
