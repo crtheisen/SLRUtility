4aa32f62a405bc345bf1f3a67cb2a245556f2208
Nuthan Munaiah <nuthan.munaiah@live.com>
2016-05-31 10:49:09 -0400

    Scholar search results now matchable

--- search.py
+++ search.py
@@ -6,10 +6,12 @@ sys.dont_write_bytecode = True
 
 
 TITLE = {
-    'acm': 6, 'dtic': 0, 'ieee': 0, 'proquest': 0, 'springer': 0, 'default': 0
+    'acm': 6, 'dtic': 0, 'ieee': 0, 'proquest': 0, 'scholar': 0, 'springer': 0,
+    'default': 0
 }
 SKIP = {
-    'acm': 1, 'dtic': 1, 'ieee': 2, 'proquest': 1, 'springer': 1, 'default': 0
+    'acm': 1, 'dtic': 1, 'ieee': 2, 'proquest': 1, 'scholar': 1, 'springer': 1,
+    'default': 0
 }
 
 
@@ -42,7 +44,7 @@ if __name__ == '__main__':
         )
     parser.add_argument(
             '-s', dest='service', default='default',
-            choices=['acm', 'dtic', 'ieee', 'proquest', 'springer'],
+            choices=['acm', 'dtic', 'ieee', 'proquest', 'scholar', 'springer'],
             help=(
                 'The indexing service from which the results were exported. '
                 'Default is "default".'
