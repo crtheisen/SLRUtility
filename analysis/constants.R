4ff5140a0ecc4e44280b2c974989e54419157ceb
Nuthan Munaiah <nuthan.munaiah@live.com>
2017-09-14 00:37:35 -0400

    Add new file to contain constants

---
+++ analysis/constants.R
@@ -0,0 +1,4 @@
+PAPER.CLASSES <- c("Define", "Supported Use", "Unsupported Use",
+                   "Not Relevant")
+PAPER.GRANULARITY <- c("Function", "File", "Binary", "System", "Network",
+                       "Theoretical")
