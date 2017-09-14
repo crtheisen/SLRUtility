8ebf05e6cbeffd5be7a4bbef3fc4a82947ab8ea6
Nuthan Munaiah <nuthan.munaiah@live.com>
2017-09-14 00:37:06 -0400

    Refactor paths used to read the data from

--- analysis/irr.R
+++ analysis/irr.R
@@ -11,11 +11,11 @@ init.libraries("irr")
 ##########################################
 ### Cohen's Kappa
 ##########################################
-title.classifications <- read.csv("data/title.csv", header = F)
+title.classifications <- read.csv("data/irr/title.csv", header = F)
 printline(paste("Title      ", get.cohenskappa(title.classifications)))
 
-abstract.classifications <- read.csv("data/abstract.csv", header = F)
+abstract.classifications <- read.csv("data/irr/abstract.csv", header = F)
 printline(paste("Abstract   ", get.cohenskappa(abstract.classifications)))
 
-fulltext.classifications <- read.csv("data/fulltext.csv", header = F)
+fulltext.classifications <- read.csv("data/irr/fulltext.csv", header = F)
 printline(paste("Full Text  ", get.cohenskappa(fulltext.classifications)))
